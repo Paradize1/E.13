a6a2d4c2d1a279604d59a447f88eac40c87251e6
Paradize1 <andrejsevcov946@gmail.com>
2024-04-22 18:10:51 +0300

    first

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { sum } from './math';
+
+document.write('Hello, world!');
+console.log('sum= ' + sum(2, 3));
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+
+
+  devServer: {
+    static: './dist',
+    hot: true,
+
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+    }),
+  ],
+  output: {
+    filename: 'main.js',
+  },
+  devtool: 'inline-source-map',
+
+
+};
